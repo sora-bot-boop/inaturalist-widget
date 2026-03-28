32472754babd79672754106ca9542b9632bb6629
Sora <sora-bot-boop@users.noreply.github.com>
2026-03-28 13:39:07 +0000

    Fix: Theme compatibility - use MaterialComponents instead of Material3 XML theme

--- app/src/main/java/com/naturewidget/app/MainActivity.kt
+++ app/src/main/java/com/naturewidget/app/MainActivity.kt
@@ -33,6 +33,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
+        val appContext = applicationContext
+        val repository = NatureRepository(appContext)
+        val settingsManager = SettingsManager.getInstance(appContext)
+        
         setContent {
             NatureWidgetTheme {
                 Surface(
@@ -40,10 +44,10 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     MainScreen(
-                        repository = NatureRepository(this),
-                        settingsManager = SettingsManager.getInstance(this),
+                        repository = repository,
+                        settingsManager = settingsManager,
                         onStartWidgetUpdates = {
-                            NatureWidgetWorker.enqueuePeriodic(this)
+                            NatureWidgetWorker.enqueuePeriodic(appContext)
                         }
                     )
                 }
